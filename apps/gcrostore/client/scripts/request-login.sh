6b1260743942efa375b1236c6249bcdc69ffa719
Shuhei Niita <huisintheta@gmail.com>
2023-04-02 16:44:52 +0000

    ✨ Add grostore

---
+++ apps/gcrostore/client/scripts/request-login.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env sh
+
+set -euC
+
+curl -X 'POST' \
+'http://'"$GCROSTORE_HOST"'/v0/request/login' \
+-H 'accept: application/json' \
+-H 'Content-Type: application/json' \
+-d '{
+    "user": {
+        "name": "'"$USER_NAME"'",
+        "email": "'"$USER_EMAIL"'"
+    },
+    "selenium": {
+        "url": "'"http://$SELENIUM_HOST.$POD_NAMESPACE.svc.cluster.local:4444/wd/hub"'",
+        "desired_capabilities": '"$SELENIUM_DESIRED_CAPABILITIES"'
+    }
+}'
